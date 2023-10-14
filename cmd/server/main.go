64f6d697c4b70a4611b3b178c95f45d2feb58c5e
Miguel Fernandes <migamado01@gmail.com>
2023-10-14 17:59:45 -0400

    add PORT env var

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load env vars (if env is dev load from .env)
+	env := os.Getenv("ENV")
+	if env == "dev" {
+		fmt.Println("Loading config from .env")
+		err := godotenv.Load("../../.env")
+		if err != nil {
+			fmt.Println("Error loading config: ", err.Error())
+			os.Exit(1)
+		}
+	}
 
-	log.Fatal(http.ListenAndServe(":9000", http.FileServer(http.Dir("../../assets"))))
+	port := os.Getenv("PORT")
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, http.FileServer(http.Dir("../../assets"))))
 }
